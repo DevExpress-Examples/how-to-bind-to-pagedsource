6362d7463523e8df3599a0fb0a7ad48893bc39c4
DevExpressExampleBot <fakeEmail>
2018-04-20 09:40:36 +0300

    Source auto update [skip ci]

---
+++ VB/IssuesContext/IssueSortOrder.vb
@@ -0,0 +1,8 @@
+﻿Namespace PagedSourceSample
+    Public Enum IssueSortOrder
+        [Default]
+        CreatedDescending
+        VotesAscending
+        VotesDescending
+    End Enum
+End Namespace
